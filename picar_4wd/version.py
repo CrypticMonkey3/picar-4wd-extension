de6b2246077fdf807811445425166c508e4bce1b
CrypticMonkey3 <78447236+CrypticMonkey3@users.noreply.github.com>
2024-04-08 14:50:05 +0100

    Update version.py

--- picar_4wd/version.py
+++ picar_4wd/version.py
@@ -1,1 +1,9 @@
+# / -------------------------------------------------------------------------------------- \
+# | Title: PiCar_4WD version.py source code												   |
+# | Author: Sunfounder																	   |
+# | Last update: 21 February 2024														   |
+# | Availability: https://github.com/sunfounder/picar-4wd/blob/master/picar_4wd/version.py |
+# \ -------------------------------------------------------------------------------------- /
+
+
 __version__ = "1.1.0"
